0ae410be5560013b8d1aa5a950e38bfdc09204c5
Yiqin Yao <yy1165@nyu.edu>
2016-10-20 22:55:28 +0800

    add a search bar and table view in view controller with fake search result.

---
+++ searchStore/searchResult.swift
@@ -0,0 +1,15 @@
+//
+//  searchResult.swift
+//  searchStore
+//
+//  Created by Yiqin Yao on 20/10/2016.
+//  Copyright © 2016 Yiqin Yao. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+
+}
--- searchStore/searchViewController.swift
+++ searchStore/searchViewController.swift
@@ -8,11 +8,87 @@
 
 import UIKit
 
-class searchViewController: UIViewController {
+class searchViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    var searchResults = [SearchResult]()
+    var hasSearched = false
+    var i: Int?
+    
+    
+    private struct Storyboard {
+        static let searchResultCellReuseIdentifier  = "SearchResultCellIdentifier"
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if searchBar.text! != "justin bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        hasSearched = true
+        tableView.reloadData() //The table view’s delegate or data source calls this method when it wants the table view to completely reload its data
+    }
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: Storyboard.searchResultCellReuseIdentifier)
+        
+        if cell == nil { // if there is no cell reusable...
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: Storyboard.searchResultCellReuseIdentifier)
+        }
+        
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing Found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        }
+        if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }// tap on a row it will become selected and stays selected, cancel it with animation
+    
+    func tableView(_ tableView: UITableView,
+                   willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }// if count == 0, cell shows (not found), and it cannot be selected.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0) // 20-points for the status bar and 44 points for the search bar.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +98,22 @@ class searchViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
